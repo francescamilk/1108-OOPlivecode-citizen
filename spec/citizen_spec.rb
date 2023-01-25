0dbd5ae169303e201841370835aea04064604c23
Francesca Santoriello <f.santoriello@hotmail.com>
2023-01-25 11:48:12 +0100

    sample test --fixTypo

--- spec/citizen_spec.rb
+++ spec/citizen_spec.rb
@@ -3,7 +3,7 @@ require_relative "../citizen"
 describe Citizen do
     it "exists as a Class" do
         citizen    = Citizen.new
-        class_name = cititzen.class
+        class_name = citizen.class
 
         expect(class_name).to eq (Citizen)
     end
